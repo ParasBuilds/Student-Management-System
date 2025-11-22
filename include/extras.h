43b832558776c11633a18eec737c4e4af8e8b265
PARAS RANA <parasrana8806@gmail.com>
2025-11-22 12:05:25 +0530

    Create extras.h for demo functions
    Added header file for extra demo functions.

---
+++ include/extras.h
@@ -0,0 +1,14 @@
+#ifndef EXTRAS_H
+#define EXTRAS_H
+
+/* extras.h
+   Small demo functions to cover recursion, pointers and 2D array usage.
+   These are separate so main functionality stays modular.
+*/
+
+long factorial(long n);
+void pointer_demo(void);
+void extras_demo(void);
+void marks_matrix_demo(void);
+
+#endif /* EXTRAS_H */
